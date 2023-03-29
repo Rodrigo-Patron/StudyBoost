6b12fb6ceb76933a6251a9667526e28f25cd2756
Mohammad Bayasi <mohammad.bayasi96@gmail.com>
2023-03-29 15:58:30 +0200

    Schemas added

---
+++ server/models/Teacher.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose
+
+
+// Teacher Schema
+const teacherSchema = new Schema({
+
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true },
+    schoolID: { type: Number, required: true, trim: true, unique: true },
+    appointments: [{ type: Schema.Types.ObjectId, ref: "appointment" }],
+    subject: [{ type: String, required: true, unique: true }],
+    availability: { type: Date },
+    role: { type: String, default: "teacher" }
+}, {
+    toJSON: {
+        transform: function (doc, userObj) {
+            delete userObj.password;
+            delete userObj.__v;
+            return userObj;
+        }
+    }
+})
+
+// Teacher Model
+const Teacher = model('teacher', teacherSchema)
+
+export default Teacher
