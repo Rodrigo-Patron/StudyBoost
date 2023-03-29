fb1be39a25b3be10fcfb5bbe5d12c77637744153
Rodrigo Patron <rodrigopatronf@gmail.com>
2023-03-29 14:01:49 +0200

    project structure

---
+++ server/lib/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = () => {
+  const { DB_USER, DB_PASS, DB_NAME, DB_HOST } = process.env;
+
+  const connectionString = `mongodb+srv://${DB_USER}:${DB_PASS}${DB_HOST}/${DB_NAME}`;
+
+  mongoose
+    .connect(connectionString)
+    .then(() => console.log("[DB] Connected"))
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export default connectDB;
