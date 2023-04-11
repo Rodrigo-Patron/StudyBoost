ab910a9f76adc6d3d7ba129c49e4d791f040a48b
Rodrigo Patron <rodrigopatronf@gmail.com>
2023-04-11 12:26:55 +0200

    logout component updated

--- client/src/components/Logout/Logout.jsx
+++ client/src/components/Logout/Logout.jsx
@@ -1,21 +1,31 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "../../Context.jsx";
 import "./Logout.scss";
 
 function Logout() {
   const navigate = useNavigate();
+  const { setTeacher, setStudent, setStudentToken, setTeacherToken } =
+    useContext(Context);
 
   function logoutHandler() {
     localStorage.removeItem("teacherToken");
+    localStorage.removeItem("teacher");
     localStorage.removeItem("studentToken");
+    localStorage.removeItem("student");
     navigate("/Home");
-  }
 
+    setTeacher(null);
+    setTeacherToken(null);
+    setStudent(null);
+    setStudentToken(null);
+  }
   return (
     <div className="Logout">
       <Button className="logout-btn" onClick={logoutHandler}>
-        Log out
+        Logout
       </Button>
     </div>
   );
