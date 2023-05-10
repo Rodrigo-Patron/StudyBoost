d4a16b115941334b9dd315cf0151f00232106001
Rodrigo Patron <rodrigopatronf@gmail.com>
2023-05-10 17:07:45 +0200

    SD responsiveness updated

--- client/src/components/SDashboard/SDashboard.jsx
+++ client/src/components/SDashboard/SDashboard.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import "./SDashboard.scss";
+import SHeader from "../S-Header/SHeader";
 import { useContext, useEffect, useState } from "react";
 import { Context } from "../../Context.jsx";
-import { Button, ListGroup, Form } from "react-bootstrap";
-import SHeader from "../S-Header/SHeader";
+import { Button, ListGroup, Form, Container, Row, Col } from "react-bootstrap";
+import "./SDashboard.scss";
 import { useNavigate } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 
 function SDashboard() {
   const navigate = useNavigate();
@@ -57,23 +54,17 @@ function SDashboard() {
   const remainingWidth = 12 - headerWidth;
 
   return (
-    //     <div class="d-flex bd-highlight">
-    //   <div class="p-2 w-100 bd-highlight">Flex item</div>
-    //   <div class="p-2 flex-shrink-1 bd-highlight">Flex item</div>
-    // </div>
-
     <Container fluid={true} className="dashboard-container">
       <Row className="dashboard-row">
         <Col xs="12" md={headerWidth}>
-          {" "}
           <SHeader />
         </Col>
         <Col className="appointment-container" xs="12" md={remainingWidth}>
           <div className="top-header">
-            <h4 className="title">
+            <h3 className="title">
               Hello <span className="student-name">{student.name}</span>, find a
-              teacher and book your appointment
-            </h4>
+              teacher and book an appointment
+            </h3>
             <div className="search-bar">
               <input placeholder="Search..." onChange={filterHandler} />
             </div>
